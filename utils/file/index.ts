78a6d72dc6502b74db2595ad83b2ab46b17062e0
haena-j <jhy83088@gmail.com>
2023-08-01 21:59:45 +0900

    Refactor: replace getFileIcon with getExtension

--- utils/file/index.ts
+++ utils/file/index.ts
@@ -1,5 +1,9 @@
-const getFileIcon = (file: File) => {
-  const { type } = file;
-  const icon = type.split('/')[0];
-  return icon;
+// Extract the extension from a file name.
+const getExtension = (filename: string): string => {
+  const index = filename.lastIndexOf('.');
+  return index < 1 ? '' : filename.substring(index + 1).toLowerCase();
+};
+
+export default {
+  getExtension,
 };
